28b4b276ffbdddaafec03ab72b3ce7233a266b2c
Ken Cavagnolo <ken@kcavagnolo.com>
2017-03-30 13:03:51 -0400

    Added Google Optimize properties

--- wp_content/js/ganalytics.js
+++ wp_content/js/ganalytics.js
@@ -3,4 +3,5 @@
 			 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-8034516-4', 'auto');
+ga('require', 'GTM-P8ZLBLH');
 ga('send', 'pageview');
