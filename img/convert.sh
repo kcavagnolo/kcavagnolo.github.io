5693814e44b1fea51799b36bb476054787a15900
Ken Cavagnolo <4321555+kcavagnolo@users.noreply.github.com>
2020-03-26 17:04:01 -0400

    webp trans

---
+++ img/convert.sh
@@ -0,0 +1,8 @@
+magick alc.png -quality 50 -define webp:lossless=true alc.webp
+magick bg.jpg -quality 50 -define webp:lossless=true bg.webp
+magick bv.png -quality 50 -define webp:lossless=true bv.webp
+magick digital.jpg -quality 50 -define webp:lossless=true digital.webp
+magick ihf.png -quality 50 -define webp:lossless=true ihf.webp
+magick knx.jpg -quality 50 -define webp:lossless=true knx.webp
+magick pac.png -quality 50 -define webp:lossless=true pac.webp
+magick 404.jpg -quality 50 -define webp:lossless=true 404.webp
