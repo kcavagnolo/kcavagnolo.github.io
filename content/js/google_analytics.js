dd030da769e322c035c0d760c15551d4d92b634d
Ken Cavagnolo <4321555+kcavagnolo@users.noreply.github.com>
2023-08-24 08:17:56 -0700

    updated

--- content/js/google_analytics.js
+++ content/js/google_analytics.js
@@ -1,14 +1,9 @@
-(function (i, s, o, g, r, a, m) {
-	i['GoogleAnalyticsObject'] = r;
-	i[r] = i[r] || function () {
-		(i[r].q = i[r].q || []).push(arguments)
-	}, i[r].l = 1 * new Date();
-	a = s.createElement(o),
-		m = s.getElementsByTagName(o)[0];
-	a.async = 1;
-	a.src = g;
-	m.parentNode.insertBefore(a, m)
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-ga('create', 'UA-8034516-4', 'auto');
-ga('require', 'GTM-P8ZLBLH');
-ga('send', 'pageview');
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZZXD7Q8544"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZZXD7Q8544');
+</script>
